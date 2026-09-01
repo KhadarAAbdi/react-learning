eb015fb7122f33d027cf2af368295b887b2b2e38
Mohamed Abdi <khadar.abdi165@gmail.com>
2026-09-01 22:16:11 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // App is just a shell. It renders whichever exercise we're currently working on.
 // When we move to the next exercise, swap the import + the tag below.
-import Exercise01 from './exercises/Exercise01'
+import Exercise03 from './exercises/Exercise03'
 
 export default function App() {
   return (
     <main className="app">
-      <Exercise01 />
+      <Exercise03 />
     </main>
   )
 }
